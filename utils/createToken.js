008d51a20f3f1313d9d374eeb491d475a8bf096c
iamabhas <abhas123mnm@gmail.com>
2024-03-25 12:22:26 +0545

    refactor: rename folders

--- utils/createToken.js
+++
@@ -1,8 +0,0 @@
-import config from "../config.js";
-import jwt from "jsonwebtoken";
-
-export const createAccessToken = (id, username) => {
-  return jwt.sign({ id, username }, config.accessTokenSecret, {
-    expiresIn: "15m",
-  });
-};
