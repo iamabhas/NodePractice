d41acde9927f2cda18582bf38486a01e4a5152dd
iamabhas <abhas123mnm@gmail.com>
2024-03-25 12:01:25 +0545

    refactor: remove cookie-parser middleware from index.js

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import config from "./config.js";
-import cookieParser from "cookie-parser";
 
 //Routes
 import authRouter from "./routes/authRoutes.js";
+import tasksRouter from "./routes/tasksRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
-app.use("/api/", authRouter);
+app.use("/api/", [authRouter, tasksRouter]);
 
 app.get("/", (req, res) => {
   res.json({ status: true, message: "Node JS Application" });
