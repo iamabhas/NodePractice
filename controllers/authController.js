838974dea01be08ddd127fda1c6015d23f63dce6
iamabhas <abhas123mnm@gmail.com>
2024-03-25 12:16:12 +0545

    refactor: remove set cookie while logging in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-import { createAccessToken } from "../utils/createToken.js";
+import { createAccessToken } from "../utils/createAccessToken.js";
+import sendResponse from "../utils/responseHandler.js";
 import userModel from "../models/authDB.js";
 
 export const SignUp = async (req, res) => {
@@ -6,13 +7,9 @@ export const SignUp = async (req, res) => {
 
   try {
     await userModel.userSignUp(username, password);
-    res.status(201).json({
-      message: `User '${username}' registered !`,
-    });
+    sendResponse(res, 201, "success", `User '${username}' registered !`);
   } catch (error) {
-    res
-      .status(error.statusCode || 500)
-      .json({ status: error.status, error: error.message });
+    sendResponse(res, error.statusCode || 500, "error", error.message);
   }
 };
 
@@ -22,40 +19,30 @@ export const Login = async (req, res) => {
     const user = await userModel.userLogin(username, password);
     const accessToken = createAccessToken(user._id, user.username);
 
-    res.cookie("token", accessToken, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
-
     res.status(200).json({
       message: `User '${username}' logged in !`,
       accessToken,
     });
   } catch (error) {
-    res
-      .status(error.statusCode || 500)
-      .json({ status: error.status, error: error.message });
+    sendResponse(res, error.statusCode || 500, "error", error.message);
   }
 };
 
 export const Logout = async (req, res) => {
   try {
     if (!req.user || !req.user.username) {
-      return res.status(401).json({ message: "User is not authenticated." });
+      return sendResponse(res, 401, "fail", "User is not authenticated.");
     }
 
-    res.cookie("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    });
-
     const { username } = req.user;
-    res
-      .status(200)
-      .json({ message: `User '${username}' logged out successfully!` });
+    sendResponse(
+      res,
+      200,
+      "success",
+      `User '${username}' logged out successfully!`
+    );
   } catch (error) {
     console.error("Logout error:", error);
-    res.status(500).json({ message: "Cannot logout. Please try again!" });
+    sendResponse(res, 500, "error", "Cannot logout. Please try again!");
   }
 };
