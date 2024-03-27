aab04ce015519d1f473e9d8098d211182b33b94a
iamabhas <abhas123mnm@gmail.com>
2024-03-27 13:40:16 +0545

    feat: add role in user schema

--- models/authDB.js
+++ models/authDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import validator from "validator";
-import createAppError from "../controllers/errorController.js";
+import createAppError from "../utils/errorHandler.js";
 
 const { Schema } = mongoose;
 
@@ -15,12 +15,21 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    required: true,
+    enum: ["student", "teacher"],
+  },
 });
 
 //signup
-userSchema.statics.userSignUp = async function (username, password) {
+userSchema.statics.userSignUp = async function (username, password, role) {
   if (!username || !password) {
     throw createAppError("Either username or password is missing !", 400);
+  }
+
+  if (!role) {
+    throw createAppError("Role must me assigned while registering !", 400);
   }
 
   if (!validator.isStrongPassword(password)) {
@@ -39,7 +48,7 @@ userSchema.statics.userSignUp = async function (username, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
 
-  const user = await this.create({ username, password: hash });
+  const user = await this.create({ username, password: hash, role });
 
   return user;
 };
