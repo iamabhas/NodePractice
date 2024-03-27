b996bb03558db28fa5612b78af0f959641714fb5
iamabhas <abhas123mnm@gmail.com>
2024-03-27 13:41:42 +0545

    refactor: seperated teacher and student routes

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -5,13 +5,24 @@ import {
   getTasks,
   markAsComplete,
   deleteTask,
-} from "../controllers/tasksController.js";
+} from "../controllers/taskControllers/studentController.js";
+import {
+  getTasksForApproval,
+  markAsApproved,
+  getStudents,
+} from "../controllers/taskControllers/teacherController.js";
 
 const tasksRouter = express.Router();
 
+//student routes
 tasksRouter.post("/addTask", validateToken, addTask);
 tasksRouter.get("/getTasks", validateToken, getTasks);
 tasksRouter.patch("/completeTask/:taskId", validateToken, markAsComplete);
 tasksRouter.delete("/deleteTask/:taskId", validateToken, deleteTask);
+
+//teacher routes
+tasksRouter.get("/getAllTasks", validateToken, getTasksForApproval);
+tasksRouter.patch("/approveTask/:taskId", validateToken, markAsApproved);
+tasksRouter.get("/getStudents", validateToken, getStudents);
 
 export default tasksRouter;
