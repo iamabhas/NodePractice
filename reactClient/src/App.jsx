7fc285ce6e8a8ba6cfed4b04745932cc5affd099
iamabhas <abhas123mnm@gmail.com>
2024-03-27 14:05:28 +0545

    refactor: basic setup

--- reactClient/src/App.jsx
+++ reactClient/src/App.jsx
@@ -1,52 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import Tdo from "./Tdo";
-const myTodo = [
-  {
-    id: 1,
-    todo: "asdasd",
-    done: true,
-  },
-  {
-    id: 3,
-    todo: "addsdasd",
-    done: false,
-  },
-  {
-    id: 2,
-    todo: "asasdasd",
-    done: true,
-  },
-];
-
+import { Typography } from "@mui/material";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
-      <div>
-        <h1>Todos</h1>
-        <div>
-          {myTodo.map((t) => {
-            return <Tdo {...t} />;
-          })}
-        </div>
-      </div>
+      <Typography variant="h4">Task Manager</Typography>
     </>
   );
 }
